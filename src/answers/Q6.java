8586626b913e161e3696c0601cb743cc07a1a9c2
King George <georgeofosumensah@gmail.com>
2021-02-25 16:53:56 +0000

    I created a class called Q6 to address question 6

---
+++ src/answers/Q6.java
@@ -0,0 +1,32 @@
+package answers;
+
+class Room{
+	int roomno;
+	String roomtype;
+	double roomarea;
+	boolean ACmachine;
+	
+	Room(int no, String type, double area, boolean AC){
+		roomno = no;
+		roomtype = type;
+		roomarea = area;
+		ACmachine = AC;
+	}
+	
+	void setdata(int no, String type, double area, boolean AC){
+	
+	}
+	
+	void displaydata() {
+		
+	}
+}
+
+public class Q6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
